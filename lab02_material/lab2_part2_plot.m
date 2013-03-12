66adaa91217de570ee45921be496ce7f1ac94f62
Shir Aharon <saharon@caltech.edu>
2013-03-12 06:11:09 -0400

    Finished plotting code

--- lab02_material/lab2_part2_plot.m
+++ lab02_material/lab2_part2_plot.m
@@ -1,35 +1,58 @@
-function [] = lab2_part1_plot()
+function [] = lab2_part2_plot()
 % This function takes a data file of occupancy grid values and plots it
 
 % Define constants
 data_file = 'data.txt';
-map_image = 'lab_obs.png';
-x_size = 101;
-y_size = 101;
+map_file = 'lab_obs.png';
+x_size = 101 - 1;
+y_size = 101 - 1;
 x_start = -2.5;
 y_start = -2.5;
 grid_size = 0.05;
-
+% Compute end points
 x_end = x_start + x_size * grid_size;
 y_end = y_start + y_size * grid_size;
+% Define world size
+world_start_x = -4.0;
+world_start_y = -4.0;
+world_end_x = 4.0;
+world_end_y = 4.0;
+
+map_image = imread(map_file);
+map_image = map_image(:,:,1);
+imshow(map_image);
+axis on;
+axis([x_start x_end y_start y_end])
+hold on;
+
+% Generate x,y grid, rescale to match plotting
+% Note that x,y convention is swapped for plottng
+mesh_start_x = size(map_image, 1) * (x_start - world_start_x) / (world_end_x - world_start_x);
+mesh_end_x = size(map_image, 1) * (x_end - world_start_x) / (world_end_x - world_start_x);
+mesh_grid_x = (mesh_end_x-mesh_start_x)/x_size;
+mesh_start_y = size(map_image, 2) * (y_start - world_start_y) / (world_end_y - world_start_y);
+mesh_end_y = size(map_image, 2) * (y_end - world_start_y) / (world_end_y - world_start_y);
+mesh_grid_y = (mesh_end_y-mesh_start_y)/y_size;
+[X,Y] = meshgrid(mesh_end_y:-mesh_grid_y:mesh_start_y, mesh_start_x:mesh_grid_x:mesh_end_x);
+X = reshape(X,1,[]);
+Y = reshape(Y,1,[]);
+X = round(X);
+Y = round(Y);
 
 % Read in data
 data = csvread(data_file);
-len_data = size(data,2) / x_size
-len_data = floor(len_data)
+data = data(:,1:size(data,2)-1);
+len_data = size(data,1) / x_size;
 
-% Generate x,y grid
-[X,Y] = meshgrid(x_start:grid_size:x_end, y_start:grid_size:y_end);
-X = reshape(X,1,[]);
-Y = reshape(Y,1,[]);
-size(X)
-size(Y)
 % Plot room data
-figure;
 for t = 0:len_data-1
-    curr_data = data(1+t*x_size:(t+1)*x_size, :);
+    disp(t)
+    curr_data = data(1+t*x_size:1+(t+1)*x_size, :);
     curr_data = reshape(curr_data,1,[]);
-    size(curr_data)
-    scatter(X, Y, 2, [curr_data * 255, 0, 0]);
-    title('Room Occupancy Grid')
+    color = curr_data' * [1, 0, 0];
+    clf
+    imshow(map_image);
+    hold on;
+    scatter(X, Y, 2, color);
+    drawnow
 end
